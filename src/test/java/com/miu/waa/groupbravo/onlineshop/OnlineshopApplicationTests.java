42570792950fcf74cf995070eaab2d6ee3ae0094
Moses Wasswa <wassmoses@gmail.com>
2020-05-02 17:50:52 -0600

    Sharing the project structure to the online repository

---
+++ src/test/java/com/miu/waa/groupbravo/onlineshop/OnlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.waa.groupbravo.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
