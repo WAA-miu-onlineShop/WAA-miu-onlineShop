3f26f327202757c6484d9bb7419348ecec70f1bc
Moses Wasswa <wassmoses@gmail.com>
2020-05-04 13:18:10 -0500

    defining and adding controller classes

---
+++ src/main/java/com/miu/waa/groupbravo/onlineshop/controller/AddressController.java
@@ -0,0 +1,9 @@
+package com.miu.waa.groupbravo.onlineshop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+}
---
+++ src/main/java/com/miu/waa/groupbravo/onlineshop/controller/ProductCategoryController.java
@@ -0,0 +1,7 @@
+package com.miu.waa.groupbravo.onlineshop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductCategoryController {
+}
