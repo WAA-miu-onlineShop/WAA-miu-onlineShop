8ce6b960510dba9a403dab3b6fcae096b34c9ec5
jmebrahtu <59802086+jmebrahtu@users.noreply.github.com>
2020-05-10 22:54:02 -0500

    Implementation of cancelorder() on orderService

--- src/main/java/com/miu/waa/groupbravo/onlineshop/service/serviceImpl/OrderServiceImpl.java
+++ src/main/java/com/miu/waa/groupbravo/onlineshop/service/serviceImpl/OrderServiceImpl.java
@@ -2,12 +2,15 @@ package com.miu.waa.groupbravo.onlineshop.service.serviceImpl;
 
 import com.miu.waa.groupbravo.onlineshop.domain.EOrderStatus;
 import com.miu.waa.groupbravo.onlineshop.domain.Order;
+import com.miu.waa.groupbravo.onlineshop.domain.User;
 import com.miu.waa.groupbravo.onlineshop.repository.OrderRepository;
 import com.miu.waa.groupbravo.onlineshop.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.ByteArrayInputStream;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class OrderServiceImpl implements OrderService {
 
@@ -42,7 +45,27 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order cancelOrder(Long orderId) {
-        return null;
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+        if(!optionalOrder.isPresent())
+            throw new RuntimeException("Order does not exist");
+
+        Order order = optionalOrder.get();
+        order.setOrderStatus(getStatusByName(EOrderStatus.CANCELLED));
+
+//        if(order.getPayment().getPayment() == PaymentMethod.POINT)
+//            order.setPoint(0);
+
+        orderRepository.save(order);
+
+        User user = order.getBuyer();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Dear ").append(user.getFirstName()).append(" ").append(user.getLastName()).append("<br/><br/>");
+        stringBuilder.append("Your order #" + orderId.toString() + " on 3L* Store (%s) has been cancelled.<br/><br/>");
+        stringBuilder.append("Thank you so much<br/><br/>Waa3L Team.");
+
+       // emailService.sendEmail(stringBuilder.toString(), "Order Canceled", Arrays.asList(user.getEmail()));
+        return order;
     }
 
     @Override
