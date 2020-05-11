5dcc15a5fc07d6ed010a5061a4d7e8feb4936967
alloll <rurangwa@gmail.com>
2020-05-11 16:10:10 -0600

    exception handling

--- src/main/java/com/miu/waa/groupbravo/onlineshop/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/miu/waa/groupbravo/onlineshop/service/serviceImpl/UserServiceImpl.java
@@ -4,10 +4,13 @@ import com.miu.waa.groupbravo.onlineshop.domain.ERoleType;
 import com.miu.waa.groupbravo.onlineshop.domain.EUserStatus;
 import com.miu.waa.groupbravo.onlineshop.domain.User;
 import com.miu.waa.groupbravo.onlineshop.domain.UserRole;
+import com.miu.waa.groupbravo.onlineshop.exceptions.RequestBravoException;
 import com.miu.waa.groupbravo.onlineshop.repository.UserRepository;
 import com.miu.waa.groupbravo.onlineshop.repository.UserRoleRepository;
 import com.miu.waa.groupbravo.onlineshop.service.SequenceNumberService;
 import com.miu.waa.groupbravo.onlineshop.service.UserService;
+import com.miu.waa.groupbravo.onlineshop.service.message.ServiceMessage;
+import com.miu.waa.groupbravo.onlineshop.service.message.UserMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -34,22 +37,29 @@ public class UserServiceImpl implements UserService {
     }
 
     public User saveUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        if(user.isNew()){
-            Optional<UserRole> userRole = userRoleRepository.findById(user.getUserRole().getId());
-            user.setUserRole(userRole.get());
-            String userNumber = sequenceNumberService.getNextUserNumber(user.getUserRole().getRoleType());
-            user.setUserNumber(userNumber);
+
+
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            if (user.isNew()) {
+                Optional<UserRole> userRole = userRoleRepository.findById(user.getUserRole().getId());
+                user.setUserRole(userRole.get());
+                String userNumber = sequenceNumberService.getNextUserNumber(user.getUserRole().getRoleType());
+                user.setUserNumber(userNumber);
+
+                //user.getUserRole().getRoleType().compareTo(ERoleType.SELLER) == 0
+                if (user.getUserRole().getRoleType().compareTo(ERoleType.SELLER) == 0) {
+                    user.setUserStatus(EUserStatus.NEW);
+                    //user.setActive(0);
+                } else {
+                    user.setUserStatus(EUserStatus.APPROVED);
+                    //user.setActive(1);
+                }
+            }
+            return userRepository.save(user);
+        }catch(Exception ex){
+            throw  new RequestBravoException(ex, ServiceMessage.USER_MESSAGE_ERROR_WHILE_SAVING);
         }
-        //user.getUserRole().getRoleType().compareTo(ERoleType.SELLER) == 0
-        if(user.getUserRole().getRoleType().compareTo(ERoleType.SELLER) == 0){
-            user.setUserStatus(EUserStatus.NEW);
-            //user.setActive(0);
-        }else{
-            user.setUserStatus(EUserStatus.APPROVED);
-            //user.setActive(1);
-        }
-        return userRepository.save(user);
     }
 
 }
