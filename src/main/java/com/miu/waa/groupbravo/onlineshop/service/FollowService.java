38ba027e83847384fceec76598369cda616c4e04
jmebrahtu <59802086+jmebrahtu@users.noreply.github.com>
2020-05-14 12:49:59 -0500

    FollowService is created

---
+++ src/main/java/com/miu/waa/groupbravo/onlineshop/service/FollowService.java
@@ -0,0 +1,10 @@
+package com.miu.waa.groupbravo.onlineshop.service;
+
+import com.miu.waa.groupbravo.onlineshop.domain.Following;
+import com.miu.waa.groupbravo.onlineshop.domain.User;
+
+public interface FollowService {
+    public Following subscribSeller(User seller,User buyer);
+    public Following unSubscribSeller(User seller,User buyer);
+    public boolean isFollow(User seller,User buyer);
+}
