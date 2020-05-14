50056f59363df1c16c0c9485f160b58d572a8272
jmebrahtu <59802086+jmebrahtu@users.noreply.github.com>
2020-05-14 13:30:53 -0500

    somecode wrote on following controller

--- src/main/java/com/miu/waa/groupbravo/onlineshop/controller/FollowingController.java
+++ src/main/java/com/miu/waa/groupbravo/onlineshop/controller/FollowingController.java
@@ -1,9 +1,26 @@
 package com.miu.waa.groupbravo.onlineshop.controller;
 
+import com.miu.waa.groupbravo.onlineshop.domain.User;
+import com.miu.waa.groupbravo.onlineshop.service.FollowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/following")
 public class FollowingController {
+
+    @Autowired
+    public FollowService followService;
+
+    @GetMapping("/{sellerId}")
+    public String followSeller(@PathVariable Long sellerId, Model model) {
+        //followService.subscribSeller(sel)
+        return "something-here";
+    }
 }
